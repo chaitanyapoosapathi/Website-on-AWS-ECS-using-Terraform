820bb8e01e1609658a8f7146d26d08f2f6e80b73
chaitanya poosapathi <chaitanyaraju.1995@gmail.com>
2020-11-25 18:21:42 -0500

    changes to alb

--- alb.tf
+++ alb.tf
@@ -7,7 +7,6 @@ resource "aws_lb" "test-lb" {
   security_groups = [aws_security_group.lb.id]
 }
 
-
 resource "aws_security_group" "lb" {
   name   = "${local.prefix}-lb"
   vpc_id = aws_vpc.main.id
